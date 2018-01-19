35442aa14d6157473f755f4a5f4921988b17925f
Johannes Nam <nam.johannes@gmail.com>
2018-01-19 20:07:27 +0100

    PocketCalculator Component with buttonmatrix

---
+++ src/pocket_calculator_components/Buttonmatrix.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+class Buttonmatrix extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonmatrix: [
+        ["AC", "DEL", "(", ")"],
+        ["1", "2", "3", "+"],
+        ["4", "5", "6", "-"],
+        ["7", "8", "9", "*"],
+        ["0", ".", "=", "/"],
+      ],
+      expression: null, // put expressions in history later ...
+    };
+  }
+
+  render() {
+
+    const btn_matrix = this.state.buttonmatrix.map((buttonrow, row) => {
+
+      const btn_row = buttonrow.map((button, col) => {
+        return (<div key={row + "-" + button} className="btn">{button}</div>);
+      });
+
+      return (
+        <div key={row}>
+          {btn_row}
+        </div>
+      );
+
+    });
+
+    return (
+      <div>
+        <div>display!</div>
+        <div>{btn_matrix}</div>
+      </div>
+    );
+
+  }
+}
+
+export default PocketCalculator;
